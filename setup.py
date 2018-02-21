7eb1d1bd5bc3d444ff7f55ea605da64cd270db87
nielstron <n.muendler@web.de>
2018-02-21 12:31:05 +0100

    Another fix for setup.py
    Reference from README.rst to README.md

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='PyBLNET',
-      version='0.1.5',
+      version='0.1.6',
       description='Automate web based communication with BL-NET to UVR1611',
       author='Niels Mündler',
       author_email='n.muendler@web.de',
