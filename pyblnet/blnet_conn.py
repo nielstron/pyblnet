3a1bff3e630f97f094e2f193abd3bb75e3ad95da
nielstron <n.muendler@web.de>
2018-08-12 17:05:18 +0200

    Introduce correct blnet data parsing

--- pyblnet/blnet_conn.py
+++ pyblnet/blnet_conn.py
@@ -12,19 +12,19 @@ import struct
 from .blnet_parser import BLNETParser
 
 # Constants for the UVR Communication
-CAN_MODE = b"\xDC";
-DL_MODE = b"\xA8";
-DL2_MODE = b"\xD1";
-GET_MODE = b"\x81";
-GET_HEADER = b"\xAA";
-GET_LATEST = b"0xAB";
-READ_DATA = b"0xAC";
-END_READ = b"\xAD";
-RESET_DATA = b"\xAF";
-WAIT_TIME = b"0xBA";
-MAX_RETRYS = 10;
-DATASET_SIZE = 61;
-LATEST_SIZE = 56;
+CAN_MODE = b"\xDC"
+DL_MODE = b"\xA8"
+DL2_MODE = b"\xD1"
+GET_MODE = b"\x81"
+GET_HEADER = b"\xAA"
+GET_LATEST = b"0xAB"
+READ_DATA = 0xAC
+END_READ = b"\xAD"
+RESET_DATA = b"\xAF"
+WAIT_TIME = b"0xBA"
+MAX_RETRYS = 10
+DATASET_SIZE = 61
+LATEST_SIZE = 56
 
 class BLNETDirect(object):
     '''
@@ -49,7 +49,7 @@ class BLNETDirect(object):
         self._socket = None
         self._count = None
         self._check_mode()
-    
+
     def _check_mode(self):
         '''
         Check if Bootloader Mode is supported
@@ -58,11 +58,11 @@ class BLNETDirect(object):
         self.connect()
         self._mode = self.query(GET_MODE, 1)
         self.disconnect()
-        
+
         if self._mode in [CAN_MODE, DL2_MODE, DL_MODE]:
             return True
         raise ConnectionError('BL-Net mode is not supported')
-    
+
     def connect(self):
         '''
         Connect to bootloader via TCP
@@ -85,14 +85,14 @@ class BLNETDirect(object):
                     self._socket = None
             if self._socket is None:
                 raise ConnectionError('Could not connect to BLNET')
-    
+
     def disconnect(self):
         '''
         Disconnect from bootloader via TCP
         '''
         self._socket.close()
         self._socket = None
-    
+
     def query(self, command, length):
         '''
         Send a command to the bootloader and wait for the response
@@ -113,14 +113,14 @@ class BLNETDirect(object):
 
         self.disconnect()
         raise ConnectionError('Error while querying command {}'.format(command))
-    
+
     def start_read(self):
         '''
         Start to read on the bootloader
         @return number of frames available
         '''
         return self.get_count()
-    
+
     def get_count(self):
         '''
         Get the number of datasets in the bootloader memory
@@ -129,43 +129,43 @@ class BLNETDirect(object):
         if self._count is None:
             self.connect()
             data = self.query(GET_HEADER, 21)
-            
+
             if self.checksum(data):
                 if self._mode == CAN_MODE:
-                    frame_count = struct.unpack('!{}B'.format(len(data)),
+                    frame_count = struct.unpack('<{}B'.format(len(data)),
                                                 data)[5]
                     (type, version, timestamp, frame_count, _, start_address,
                      end_address, checksum) = struct.unpack(
-                         '!BB3sB{}s3s3sB'.format(frame_count), data)
+                         '<BB3sB{}s3s3sB'.format(frame_count), data)
                     self._address_inc = 64 * frame_count
                     self._can_frames = frame_count
                     self._actual_size = 57
                     self._fetch_size = 4 + 61 * frame_count
                 elif self._mode == DL_MODE:
-                    (_, device, start_address, end_address, 
-                     checksum) = struct.unpack('!5sB3s3sB')
+                    (_, device, start_address, end_address,
+                     checksum) = struct.unpack('<5sB3s3sB')
                     self._address_inc = 64
                     self._can_frames = 1
                     self._actual_size = 57
                     self._fetch_size = 65
                 elif self._mode == DL2_MODE:
                     (_, device, start_address, end_address,
-                     checksum) = struct.unpack('!5s2s3s3sB')
+                     checksum) = struct.unpack('<5s2s3s3sB')
                     self._address_inc = 128
                     self._can_frames = 1
                     self._actual_size = 113
                     self._fetch_size = 126
- 
+
                 self._address_end = ((0x07FFFF // self._address_inc)
                                        * self._address_inc)
- 
+
                 # check address validity
                 if (start_address != b'0xFFFFFF' and
                     end_address != b'0xFFFFFF'):
                     start_address = int.from_bytes(start_address,
-                                                   byteorder='big')
+                                                   byteorder='little')
                     end_address = int.from_bytes(end_address,
-                                                 byteorder='big')
+                                                 byteorder='little')
                     # fix addresses
                     if end_address > start_address:
                         # calculate count
@@ -175,7 +175,7 @@ class BLNETDirect(object):
                         self._count = ((self._address_end + start_address
                                         - end_address) / self._address_inc)
                     self._address = end_address
-        return self._count        
+        return self._count
 
     def checksum(self, data):
         '''
@@ -183,38 +183,41 @@ class BLNETDirect(object):
         @param byte string $data Binary string to check
         @return boolean
         '''
-        binary = struct.unpack('!{}B'.format(len(data)), data)
+        binary = struct.unpack('<{}B'.format(len(data)), data)
         checksum = binary[-1]
-        
+
         sum = 0
         # build sum
         for byte in binary[:-1]:
             sum += byte
         # verify
         return sum % 256 == checksum
-    
+
     def fetch_data(self):
         '''
         Fetch datasets from bootloader memory
         @throws: ConnectionError Data could not be retreived
         @return Parser
-        ''' 
-        if self._count > 0:
+        '''
+        if self._count and self._count > 0:
             self.connect()
-            
+
             # build address for bootloader
             addresses = [
                 self._address & 0xFF,
                 (self._address & 0x7F00) >> 7,
                 (self._address & 0xFF8000) >> 15
             ]
-            
+
             # build command
-            command = struct.pack('!6B', READ_DATA, addresses[0], addresses[1],
-                                  addresses[2], 1, READ_DATA+1+sum(addresses))
-            
+            arry = ['!6B', READ_DATA, addresses[0], addresses[1], addresses[2], 1, 
+                (READ_DATA + 1 + sum(addresses)) & 0xFF ]
+            command = struct.pack(
+                '!6B', READ_DATA, addresses[0], addresses[1], addresses[2], 1, 
+                (READ_DATA + 1 + sum(addresses)) & 0xFF )
+
             data = self.query(command, self._fetch_size)
-            
+
             if self.checksum(data):
                 # increment address
                 self._address -= self._address_inc
@@ -224,7 +227,7 @@ class BLNETDirect(object):
                 return self._split_datasets(data)
             raise ConnectionError('Could not retreive data')
 
-    def _split_datasets(self, data):        
+    def _split_datasets(self, data):
         '''
         split binary string in datasets and parse dataset values
         @param data: byte string
@@ -249,12 +252,12 @@ class BLNETDirect(object):
             )
 
         start = 0
- 
+
         if self._mode == CAN_MODE:
             start = 3
         if data[start:start+DATASET_SIZE-6] == b'0x00' * (DATASET_SIZE-6):
             return False
         else:
             return {k: v.to_dict() for k, v in frames.items()}
-            
+
 
--- pyblnet/blnet_parser.py
+++ pyblnet/blnet_parser.py
@@ -10,25 +10,25 @@ import struct
 
 # Parser constant
 # 1 bit
-DIGITAL_ON = 1;
-DIGITAL_OFF = 0;
+DIGITAL_ON = 1
+DIGITAL_OFF = 0
 # 8 bit
-SPEED_ACTIVE = 0x80;
-SPEED_MASK = 0x1F;
+SPEED_ACTIVE = 0x80
+SPEED_MASK = 0x1F
 # 16 bit
-INT16_POSITIVE_MASK = 0xFFFF;
-SIGN_BIT = 0x8000;
-POSITIVE_VALUE_MASK = 0x0FFF;
-TYPE_MASK = 0x7000;
-TYPE_NONE = 0x0000;
-TYPE_DIGITAL = 0x1000;
-TYPE_TEMP = 0x2000;
-TYPE_VOLUME = 0x3000;
-TYPE_RADIATION = 0x4000;
-TYPE_RAS = 0x7000;
-RAS_POSITIVE_MASK = 0x01FF;
+INT16_POSITIVE_MASK = 0xFFFF
+SIGN_BIT = 0x8000
+POSITIVE_VALUE_MASK = 0x0FFF
+TYPE_MASK = 0x7000
+TYPE_NONE = 0x0000
+TYPE_DIGITAL = 0x1000
+TYPE_TEMP = 0x2000
+TYPE_VOLUME = 0x3000
+TYPE_RADIATION = 0x4000
+TYPE_RAS = 0x7000
+RAS_POSITIVE_MASK = 0x01FF
 # 32 bit
-INT32_MASK = 0xFFFFFFFF;
+INT32_MASK = 0xFFFFFFFF
 INT32_SIGN = 0x80000000
 
 class BLNETParser:
@@ -43,15 +43,20 @@ class BLNETParser:
         # (fetched from bootloader storage)
         if len(data) == 61:
             (_, seconds, minutes, hours, days, months, years) = struct.unpack(
-              '!55sBBBBBB', data)
+          '<55sBBBBBB', data)
             self.date = "20{:02d}-{:02d}-{:02d} {:02d}:{:02d}:{:02d}".format(
                 years, months, days, hours, minutes, seconds)
             
-        (_, digital, speed, active, power1, kWh1,
-         MWh1, power2, kWh2, MWh2) = struct.unpack(
-             '!32sH4sBLHHLHH', data)
+        # Only parse preceding data
+        data = data[:55]
+        power = [0,0]
+        kWh = [0,0]
+        MWh = [0,0]
+        (_, digital, speed, active, power[0], kWh[0],
+         MWh[0], power[1], kWh[1], MWh[1]) = struct.unpack(
+             '<32sH4sBLHHLHH', data)
         
-        analog = struct.unpack('!{}{}'.format('H'*16, 'x'*(len(data)-32)))
+        analog = struct.unpack('<{}{}'.format('H'*16, 'x'*(len(data)-32)), data)
 
         self.analog = {}
         for channel in range(0, 16):
@@ -59,11 +64,22 @@ class BLNETParser:
         
         self.digital = {}
         for channel in range(0, 16):
-            self.digital[channel] = self._convert_digital(digital[channel])
+            self.digital[channel] = self._convert_digital(digital, channel)
 
         self.speed = {}
         for channel in range(0, 4):
             self.speed[channel] = self._convert_speed(speed[channel])
+        
+        self.energy = {}
+        for channel in range(0, 2):
+            self.energy[channel] = self._convert_energy(
+                MWh[channel], kWh[channel], active, channel
+            )
+
+        self.power = {}
+        for channel in range(0, 2):
+            self.power[channel] = self._convert_power(power[channel], active,
+                                                  channel)
         
 
     def to_dict(self):
@@ -72,3 +88,84 @@ class BLNETParser:
         @return dict
         '''
         return self.__dict__
+    
+    def _convert_analog(self, value):
+        '''
+        Convert int to correct float
+        @param value: short unsigned int that was returned by blnet
+        @return float with correct sensor value
+        '''
+       
+        mask = value & TYPE_MASK
+        if mask == TYPE_TEMP:
+            return self._calculate_value(value, 0.1)
+        elif mask == TYPE_VOLUME:
+            return self._calculate_value(value, 4)
+        elif mask == TYPE_DIGITAL:
+            if value & SIGN_BIT:
+                return 1
+            else:
+                return 0
+        elif mask == TYPE_RAS:
+            return self._calculate_value(value, 0.1, RAS_POSITIVE_MASK)
+        elif mask in [TYPE_RADIATION, TYPE_NONE] or True:
+            return self._calculate_value(value)
+
+    def _convert_digital(self, value, position):
+        '''
+        Check if bit at given position is set (=1)
+        '''
+        if value & (0x1 << (position)):
+            return DIGITAL_ON
+        else:
+            return DIGITAL_OFF
+
+    def _convert_speed(self, value):
+        '''
+        Check if speed is activated and return its value
+        '''
+        if value & SPEED_ACTIVE:
+            return None
+        else:
+            return value & SPEED_MASK
+ 
+    def _convert_energy(self, mwh, kwh, active, position):
+        '''
+        Check if heat meter is activated on a given position
+        @return its energy
+        '''
+        if active & position:
+            kwh = self._calculate_value(kwh, 0.1, INT16_POSITIVE_MASK)
+            return mwh*1000 + kwh
+        else:
+            return None
+    
+    def _convert_power(self, value, active, position):
+        '''
+        checks if heat meter is activated at given position
+        @return its power
+        '''
+        if active & position:
+            return self._calculate_value(value, 1/2560, INT32_MASK, INT32_SIGN)
+        else:
+            return None
+    
+    def _calculate_value(self, value, multiplier=1, 
+                         positive_mask=POSITIVE_VALUE_MASK, signbit=SIGN_BIT):
+        result = value & positive_mask
+        if value & signbit:
+            result = - ((result ^ positive_mask) + 1)
+        return result * multiplier
+   
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        
