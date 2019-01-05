b6ba63c9033efa35cdb088d0c4582919b52a0d6a
Niels Mündler <n.muendler@tum.de>
2019-01-05 02:38:25 +0100

    Extract file contents, improve mock server

---
+++ pyblnet/tests/blnet_mock_server.py
@@ -0,0 +1,94 @@
+import os
+import urllib
+from http.server import HTTPStatus, SimpleHTTPRequestHandler, HTTPServer
+import html
+
+
+class BLNETServer(HTTPServer):
+
+    logged_in = '2DA1'
+
+
+class BLNETRequestHandler(SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        """
+        Handle get request, but check for errors in protocol
+        :return:
+        """
+        path = self.translate_path(self.path)
+
+    def send_head(self):
+        """Common code for GET and HEAD commands.
+
+        This sends the response code and MIME headers.
+
+        Return value is either a file object (which has to be copied
+        to the outputfile by the caller unless the command was HEAD,
+        and must be closed by the caller under all circumstances), or
+        None, in which case the caller has nothing further to do.
+
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                             parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            # BLNET has main.html/main.htm
+            for index in "main.html", "main.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+        try:
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
+
+    def send_error(self, code, message=None, explain=None):
+        self.log_error("code %d, message %s", code, message)
+        self.send_response(code, message)
+        self.send_header('Connection', 'close')
+
+        # Message body is omitted for cases described in:
+        #  - RFC7230: 3.3. 1xx, 204(No Content), 304(Not Modified)
+        #  - RFC7231: 6.3.6. 205(Reset Content)
+        body = None
+        if (code >= 200 and
+                code not in (HTTPStatus.NO_CONTENT,
+                             HTTPStatus.RESET_CONTENT,
+                             HTTPStatus.NOT_MODIFIED)):
+            # HTML encode to prevent Cross Site Scripting attacks
+            # (see bug #1100201)
+            # Specialized error method for BLNET
+            with open("BL-Net Zugang verweigert.html") as file:
+                body = file.read()
+            self.send_header("Content-Type", self.error_content_type)
+            self.send_header('Content-Length', int(len(body)))
+        self.end_headers()
+
+        if self.command != 'HEAD' and body:
+            self.wfile.write(body)
